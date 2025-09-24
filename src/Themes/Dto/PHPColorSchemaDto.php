93138dc13f816bb8826cb726d71b5c995970772e
Sergey Komlev <40078038+sergeykomlev@users.noreply.github.com>
2025-09-24 04:48:25 +0300

    Update PHPColorSchemaDto.php

--- src/Themes/Dto/PHPColorSchemaDto.php
+++ src/Themes/Dto/PHPColorSchemaDto.php
@@ -4,16 +4,55 @@ namespace Demyanovs\PHPHighlight\Themes\Dto;
 
 class PHPColorSchemaDto
 {
+    /**
+     * @var string
+     */
+    private $defaultColor;
+
+    /**
+     * @var string
+     */
+    private $commentColor;
+
+    /**
+     * @var string
+     */
+    private $htmlColor;
+
+    /**
+     * @var string
+     */
+    private $keywordColor;
+
+    /**
+     * @var string
+     */
+    private $stringColor;
+
+    /**
+     * PHPColorSchemaDto constructor.
+     *
+     * @param string $defaultColor
+     * @param string $commentColor
+     * @param string $htmlColor
+     * @param string $keywordColor
+     * @param string $stringColor
+     */
     public function __construct(
-        private readonly string $defaultColor,
-        private readonly string $commentColor,
-        private readonly string $htmlColor,
-        private readonly string $keywordColor,
-        private readonly string $stringColor,
+        $defaultColor,
+        $commentColor,
+        $htmlColor,
+        $keywordColor,
+        $stringColor
     ) {
+        $this->defaultColor  = $defaultColor;
+        $this->commentColor  = $commentColor;
+        $this->htmlColor     = $htmlColor;
+        $this->keywordColor  = $keywordColor;
+        $this->stringColor   = $stringColor;
     }
 
-    public function applyColors(): void
+    public function applyColors()
     {
         ini_set('highlight.default', $this->defaultColor);
         ini_set('highlight.comment', $this->commentColor);
